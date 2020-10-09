45b2c8180fe590f0a85227da85c069046a1e43a5
ikcilrep <frajerzycki@protonmail.com>
2020-10-09 16:37:29 +0200

    Implement bit cycling

--- src/bit_cycling.rs
+++ src/bit_cycling.rs
@@ -1,21 +1,47 @@
 use num_bigint::BigUint;
 use num_traits::ToPrimitive;
 
-pub fn cycle_left(data: &[i8], bits_to_shift: &BigUint, cycled_data: &mut [u8]) {}
+pub fn cycle_left(data: &[i8], bits_to_shift: &BigUint, cycled_data: &mut [u8]) {
+    let l1 = bits_to_shift % (8u64 * data.len() as u64);
+    let l2 = (&l1 % 8usize).to_usize().unwrap();
+    let l3 = (l1 / 8usize).to_usize().unwrap();
+
+    for k in 0..data.len() - l3 - 1 {
+        cycled_data[k] = exchange_bits_left(data[k + l3], data[k + l3 + 1], l2);
+    }
+    cycled_data[data.len() - 1 - l3] = exchange_bits_left(data[data.len() - 1], data[0], l2);
+    for k in data.len() - l3..data.len() {
+        cycled_data[k] =
+            exchange_bits_left(data[k + l3 - data.len()], data[k + l3 - data.len() + 1], l2);
+    }
+}
+
+#[inline]
+fn exchange_bits_left(a: i8, b: i8, bits_to_exchange: usize) -> u8 {
+    ((a as u8) << bits_to_exchange) | ((b as u8) >> (8 - bits_to_exchange))
+}
+
+#[inline]
+fn exchange_bits_right(a: u8, b: u8, bits_to_exchange: usize) -> i8 {
+    ((a << (8 - bits_to_exchange)) | (b >> bits_to_exchange)) as i8
+}
 
 pub fn cycle_right(data: &[u8], bits_to_shift: &BigUint, cycled_data: &mut [i8]) {
     let l1 = bits_to_shift % (8u64 * data.len() as u64);
     let l2 = (&l1 % 8usize).to_usize().unwrap();
     let l3 = (l1 / 8usize).to_usize().unwrap();
 
-    cycled_data[0] = (data[data.len() - 1 - l3] << (8 - l2) | data[data.len() - l3] >> l2) as i8;
-    for k in 1..l3 {
+    for k in 0..l3 {
         cycled_data[k] =
-            (data[data.len() + k - l3 - 1] << (8 - l2) | data[data.len() + k - l3] >> l2) as i8;
+            exchange_bits_right(data[data.len() + k - l3 - 1], data[data.len() + k - l3], l2);
+        //            ((data[data.len() + k - l3 - 1] << (8 - l2)) | (data[data.len() + k - l3] >> l2)) as i8;
     }
 
-    for k in l3..data.len() {
-        cycled_data[k] = (data[k - l3 - 1] << (8 - l2) | data[k - l3] >> l2) as i8;
+    cycled_data[l3] = exchange_bits_right(data[data.len() - 1], data[0], l2);
+
+    for k in l3 + 1..data.len() {
+        //cycled_data[k] = ((data[k - l3 - 1] << (8 - l2)) | (data[k - l3] >> l2)) as i8;
+        cycled_data[k] = exchange_bits_right(data[k-l3-1], data[k-l3], l2);
     }
 }
 
