b9337a5f78d8176395ef483bf811fcd602e359e5
ikcilrep <frajerzycki@protonmail.com>
2020-10-09 16:50:54 +0200

    Fix cycling by only whole bytes

--- src/bit_cycling.rs
+++ src/bit_cycling.rs
@@ -5,14 +5,23 @@ pub fn cycle_left(data: &[i8], bits_to_shift: &BigUint, cycled_data: &mut [u8]) 
     let l1 = bits_to_shift % (8u64 * data.len() as u64);
     let l2 = (&l1 % 8usize).to_usize().unwrap();
     let l3 = (l1 / 8usize).to_usize().unwrap();
-
-    for k in 0..data.len() - l3 - 1 {
-        cycled_data[k] = exchange_bits_left(data[k + l3], data[k + l3 + 1], l2);
-    }
-    cycled_data[data.len() - 1 - l3] = exchange_bits_left(data[data.len() - 1], data[0], l2);
-    for k in data.len() - l3..data.len() {
-        cycled_data[k] =
-            exchange_bits_left(data[k + l3 - data.len()], data[k + l3 - data.len() + 1], l2);
+    if l2 == 0 {
+        for k in 0..data.len() - l3 - 1 {
+            cycled_data[k] = data[k + l3] as u8;
+        }
+        cycled_data[data.len() - 1 - l3] = data[data.len() - 1] as u8;
+        for k in data.len() - l3..data.len() {
+            cycled_data[k] = data[k + l3 - data.len()] as u8;
+        }
+    } else {
+        for k in 0..data.len() - l3 - 1 {
+            cycled_data[k] = exchange_bits_left(data[k + l3], data[k + l3 + 1], l2);
+        }
+        cycled_data[data.len() - 1 - l3] = exchange_bits_left(data[data.len() - 1], data[0], l2);
+        for k in data.len() - l3..data.len() {
+            cycled_data[k] =
+                exchange_bits_left(data[k + l3 - data.len()], data[k + l3 - data.len() + 1], l2);
+        }
     }
 }
 
@@ -31,17 +40,27 @@ pub fn cycle_right(data: &[u8], bits_to_shift: &BigUint, cycled_data: &mut [i8])
     let l2 = (&l1 % 8usize).to_usize().unwrap();
     let l3 = (l1 / 8usize).to_usize().unwrap();
 
-    for k in 0..l3 {
-        cycled_data[k] =
-            exchange_bits_right(data[data.len() + k - l3 - 1], data[data.len() + k - l3], l2);
-        //            ((data[data.len() + k - l3 - 1] << (8 - l2)) | (data[data.len() + k - l3] >> l2)) as i8;
-    }
+    if l2 == 0 {
+        for k in 0..l3 {
+            cycled_data[k] = data[data.len() + k - l3] as i8;
+        }
 
-    cycled_data[l3] = exchange_bits_right(data[data.len() - 1], data[0], l2);
+        cycled_data[l3] = data[0] as i8;
 
-    for k in l3 + 1..data.len() {
-        //cycled_data[k] = ((data[k - l3 - 1] << (8 - l2)) | (data[k - l3] >> l2)) as i8;
-        cycled_data[k] = exchange_bits_right(data[k-l3-1], data[k-l3], l2);
+        for k in l3 + 1..data.len() {
+            cycled_data[k] = data[k - l3] as i8;
+        }
+    } else {
+        for k in 0..l3 {
+            cycled_data[k] =
+                exchange_bits_right(data[data.len() + k - l3 - 1], data[data.len() + k - l3], l2);
+        }
+
+        cycled_data[l3] = exchange_bits_right(data[data.len() - 1], data[0], l2);
+
+        for k in l3 + 1..data.len() {
+            cycled_data[k] = exchange_bits_right(data[k - l3 - 1], data[k - l3], l2);
+        }
     }
 }
 
