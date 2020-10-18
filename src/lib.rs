83d3e2e3e8f32c6eb954bf568ef2e637e234a07f
ikcilrep <frajerzycki@protonmail.com>
2020-10-18 22:39:49 +0200

    Create tests for encrypt and decrypt

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+use num_bigint::BigUint;
+use js_sys::Uint8Array;
+
 mod tests;
 mod blocks;
 mod bit_cycling;
+
+fn encrypt(data: &Uint8Array, key: &BigUint) -> Uint8Array {
+    return Uint8Array::new_with_length(data.length() as u32);
+}
+
+fn decrypt(data: &Uint8Array, key: &BigUint) -> Uint8Array {
+    return Uint8Array::new_with_length(data.length() as u32);
+}
