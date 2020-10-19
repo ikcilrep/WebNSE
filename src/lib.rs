56489f7aafcf266b7031fdd6ddcd8cda2ffb3d84
ikcilrep <frajerzycki@protonmail.com>
2020-10-19 20:05:54 +0200

    Implement hashing a number int a number

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,19 @@
-use num_bigint::BigUint;
 use js_sys::Uint8Array;
+use num_bigint::BigUint;
+use sha2::{Digest, Sha256};
 
-mod tests;
-mod blocks;
 mod bit_cycling;
+mod blocks;
+mod tests;
+
+fn hash_number(number: &BigUint) -> BigUint {
+    let mut hasher = Sha256::default();
+    hasher.update(number.to_bytes_be());
+
+    let output = hasher.finalize();
+
+    return BigUint::from_bytes_be(&output);
+}
 
 fn encrypt(data: &Uint8Array, key: &BigUint) -> Uint8Array {
     return Uint8Array::new_with_length(data.length() as u32);
