f88618c4e91b01080e545e52adc42372ee9e08d2
ikcilrep <frajerzycki@protonmail.com>
2020-10-27 19:40:19 +0100

    Use Uint8Array as output instead of slice

--- src/blocks/bytes.rs
+++ src/blocks/bytes.rs
@@ -1,4 +1,5 @@
 use crate::blocks::ELEMENT_SIZE;
+use js_sys::Uint8Array;
 
 fn u40_as_i40(u40: u64) -> i64 {
     let half = 1 << (8 * ELEMENT_SIZE - 1);
@@ -31,15 +32,15 @@ pub fn join_bytes(bytes: &[u8], data: &mut [i64]) {
     }
 }
 
-pub fn split_bytes<I>(data: &mut I, bytes: &mut [u8])
+pub fn split_bytes<I>(data: &mut I, bytes: &Uint8Array)
 where
     I: Iterator<Item = i64>,
 {
-    for i in (0..bytes.iter().count()).step_by(ELEMENT_SIZE) {
+    for i in (0..bytes.length()).step_by(ELEMENT_SIZE) {
         let mut e = i40_as_u40(data.next().unwrap());
 
-        for j in i..i + ELEMENT_SIZE {
-            bytes[j] = (e & 255) as u8;
+        for j in i..i + ELEMENT_SIZE as u32 {
+            bytes.set_index(j, (e & 255) as u8);
             e >>= 8;
         }
         if e > 1 {
@@ -51,16 +52,16 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::blocks::ELEMENT_SIZE;
     use crate::blocks::BLOCK_SIZE;
+    use crate::blocks::ELEMENT_SIZE;
 
     #[test]
     fn join_bytes_can_be_reversed() {
         let data: [i64; BLOCK_SIZE] = [274877906943; BLOCK_SIZE];
-        let mut bytes = [0; BLOCK_SIZE * ELEMENT_SIZE];
+        let mut bytes = Uint8Array::new_with_length(BLOCK_SIZE as u32 * ELEMENT_SIZE as u32);
         split_bytes(&mut data.iter().map(|&x| x as i64), &mut bytes);
         let mut joined_bytes = [0; BLOCK_SIZE];
-        join_bytes(&bytes, &mut joined_bytes);
+        join_bytes(&bytes.to_vec(), &mut joined_bytes);
     }
 
     #[test]
--- src/blocks/mod.rs
+++ src/blocks/mod.rs
@@ -5,9 +5,10 @@ use crate::blocks::bytes::join_bytes;
 use crate::blocks::bytes::split_bytes;
 use crate::blocks::generation::derive_key;
 use crate::blocks::generation::generate_iv;
+use js_sys::Uint8Array;
 use num_bigint::BigUint;
-use rand::rngs::OsRng;
-use rand::RngCore;
+use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen_test::*;
 
 pub const BLOCK_SIZE: usize = 256;
 const SALT_SIZE: usize = 16;
@@ -16,17 +17,24 @@ const ELEMENT_SIZE: usize = 5;
 
 pub const ENCRYPTED_BLOCK_SIZE: usize = ELEMENT_SIZE * BLOCK_SIZE + BLOCK_SIZE + SALT_SIZE;
 
-pub fn encrypt_block(block: &[i8], key: &BigUint, encrypted_block: &mut [u8; ENCRYPTED_BLOCK_SIZE]) {
-    let salt = &mut encrypted_block[0..SALT_SIZE];
-    OsRng.fill_bytes(salt);
+#[wasm_bindgen(module = "crypto")]
+extern "C" {
+    fn randomBytes(size: u32) -> Uint8Array;
+}
+
+pub fn encrypt_block(block: &[i8], key: &BigUint, encrypted_block: &Uint8Array) {
+    let salt = randomBytes(SALT_SIZE as u32);
+    for i in 0..salt.length() {
+        encrypted_block.set_index(i, salt.get_index(i));
+    }
 
     let mut derived_key = [0; BLOCK_SIZE];
-    derive_key(key, &salt, &mut derived_key);
+    derive_key(key, &salt.to_vec(), &mut derived_key);
 
     let mut iv = [0; BLOCK_SIZE];
     generate_iv(&derived_key, &block.to_vec(), &mut iv);
     for i in SALT_SIZE..SALT_SIZE + BLOCK_SIZE {
-        encrypted_block[i] = iv[i - SALT_SIZE] as u8;
+        encrypted_block.set_index(i as u32, iv[i - SALT_SIZE] as u8);
     }
 
     let mut sum1 = 0;
@@ -44,7 +52,10 @@ pub fn encrypt_block(block: &[i8], key: &BigUint, encrypted_block: &mut [u8; ENC
         .map(|(&r, &p)| r as i64 * sum1 - (p as i64 * sum2));
     split_bytes(
         &mut encrypted_block_iter,
-        &mut encrypted_block[SALT_SIZE + BLOCK_SIZE..ENCRYPTED_BLOCK_SIZE],
+        &encrypted_block.subarray(
+            SALT_SIZE as u32 + BLOCK_SIZE as u32,
+            ENCRYPTED_BLOCK_SIZE as u32,
+        ),
     );
 }
 
@@ -89,6 +100,47 @@ fn decrypt_block(encrypted_block: &[u8], key: &BigUint, decrypted_block: &mut [i
     }
 }
 
+#[wasm_bindgen_test]
+pub fn encrypt_block_can_be_reversed() {
+    use std::str::FromStr;
+
+    let unsigned_block: [u8; BLOCK_SIZE] = [
+        237, 252, 84, 64, 120, 86, 39, 29, 40, 209, 77, 44, 108, 122, 150, 132, 46, 92, 98, 25,
+        173, 186, 243, 142, 77, 145, 76, 71, 245, 118, 52, 172, 221, 109, 180, 222, 235, 18, 182,
+        237, 67, 240, 184, 164, 150, 90, 193, 97, 89, 74, 204, 205, 185, 255, 80, 49, 97, 172, 213,
+        235, 96, 46, 24, 104, 68, 32, 179, 110, 229, 157, 134, 6, 94, 199, 82, 118, 185, 155, 160,
+        157, 240, 102, 63, 131, 3, 195, 152, 146, 202, 243, 217, 208, 3, 93, 180, 20, 164, 129,
+        112, 207, 162, 16, 69, 220, 173, 220, 211, 162, 84, 14, 167, 182, 91, 110, 178, 214, 31,
+        152, 103, 133, 191, 213, 244, 226, 49, 21, 15, 36, 21, 122, 54, 114, 121, 210, 134, 219, 7,
+        220, 110, 12, 111, 66, 28, 104, 217, 18, 120, 177, 188, 145, 244, 194, 16, 187, 34, 147,
+        164, 94, 247, 204, 192, 54, 143, 155, 66, 191, 225, 159, 88, 20, 25, 12, 72, 178, 212, 178,
+        61, 85, 108, 223, 38, 187, 44, 187, 197, 138, 143, 180, 248, 48, 132, 157, 193, 104, 196,
+        186, 26, 198, 214, 182, 119, 65, 187, 161, 108, 234, 95, 112, 36, 19, 42, 194, 95, 18, 45,
+        154, 203, 218, 118, 20, 185, 197, 197, 85, 42, 147, 251, 18, 197, 192, 107, 156, 191, 115,
+        194, 207, 162, 16, 69, 220, 173, 220, 211, 162, 84, 14, 167, 182, 91, 110, 178, 214, 31,
+        152, 103, 133,
+    ];
+    let mut block = Vec::new();
+
+    for i in 0..BLOCK_SIZE {
+        block.push(unsigned_block[i] as i8);
+    }
+
+    let key = BigUint::from_str("110192826829776194000614388426091705128").unwrap();
+
+    let mut encrypted_block = Uint8Array::new_with_length(ENCRYPTED_BLOCK_SIZE as u32);
+
+    encrypt_block(&block, &key, &mut encrypted_block);
+
+    let mut decrypted_block = [1; BLOCK_SIZE];
+
+    decrypt_block(&encrypted_block.to_vec(), &key, &mut decrypted_block);
+
+    for (e1, e2) in block.iter().zip(decrypted_block.iter()) {
+        assert_eq!(e1, e2);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -96,34 +148,5 @@ mod tests {
     #[test]
     fn encrypted_block_size_is_1552() {
         assert_eq!(ENCRYPTED_BLOCK_SIZE, 1552);
-    }
-
-    #[test]
-    fn encrypt_block_can_be_reversed() {
-        use num_bigint::RandBigInt;
-        use rand::{thread_rng, RngCore};
-
-        let mut rng = thread_rng();
-        let mut unsigned_block = [0; BLOCK_SIZE];
-        rng.fill_bytes(&mut unsigned_block);
-        let mut block = Vec::new();
-
-        for i in 0..BLOCK_SIZE {
-            block.push(unsigned_block[i] as i8);
-        }
-
-        let key = rng.gen_biguint(128);
-
-        let mut encrypted_block = [0; ENCRYPTED_BLOCK_SIZE];
-
-        encrypt_block(&block, &key, &mut encrypted_block);
-
-        let mut decrypted_block = [1; BLOCK_SIZE];
-
-        decrypt_block(&encrypted_block.to_vec(), &key, &mut decrypted_block);
-
-        for (e1, e2) in block.iter().zip(decrypted_block.iter()) {
-            assert_eq!(e1, e2);
-        }
     }
 }
