5318a037f4a45a7050ed17edc4aa3c771e176091
ikcilrep <frajerzycki@protonmail.com>
2020-10-30 20:40:34 +0100

    Make generate_iv test wasm_bindgen

--- src/blocks/generation/mod.rs
+++ src/blocks/generation/mod.rs
@@ -1,13 +1,14 @@
 mod vectors;
 
-use js_sys::Uint8Array;
 use crate::blocks::generation::vectors::are_orthogonal;
 use crate::blocks::generation::vectors::vector_difference;
 use crate::blocks::BLOCK_SIZE;
 use hkdf::Hkdf;
+use js_sys::Uint8Array;
 use num_bigint::BigUint;
 use sha2::Sha256;
 use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen_test::*;
 
 #[wasm_bindgen(module = "crypto")]
 extern "C" {
@@ -57,11 +58,44 @@ pub fn generate_iv(derived_key: &[u16; BLOCK_SIZE], block: &[i8], iv: &mut [i8; 
     } {}
 }
 
+#[wasm_bindgen_test]
+pub fn generate_iv_derived_key_is_not_orthogonal_with_block_and_key_difference() {
+    let derived_key = [1; BLOCK_SIZE];
+
+    let unsigned_block: [u8; BLOCK_SIZE] = [
+        237, 252, 84, 64, 120, 86, 39, 29, 40, 209, 77, 44, 108, 122, 150, 132, 46, 92, 98, 25,
+        173, 186, 243, 142, 77, 145, 76, 71, 245, 118, 52, 172, 221, 109, 180, 222, 235, 18, 182,
+        237, 67, 240, 184, 164, 150, 90, 193, 97, 89, 74, 204, 205, 185, 255, 80, 49, 97, 172, 213,
+        235, 96, 46, 24, 104, 68, 32, 179, 110, 229, 157, 134, 6, 94, 199, 82, 118, 185, 155, 160,
+        157, 240, 102, 63, 131, 3, 195, 152, 146, 202, 243, 217, 208, 3, 93, 180, 20, 164, 129,
+        112, 207, 162, 16, 69, 220, 173, 220, 211, 162, 84, 14, 167, 182, 91, 110, 178, 214, 31,
+        152, 103, 133, 191, 213, 244, 226, 49, 21, 15, 36, 21, 122, 54, 114, 121, 210, 134, 219, 7,
+        220, 110, 12, 111, 66, 28, 104, 217, 18, 120, 177, 188, 145, 244, 194, 16, 187, 34, 147,
+        164, 94, 247, 204, 192, 54, 143, 155, 66, 191, 225, 159, 88, 20, 25, 12, 72, 178, 212, 178,
+        61, 85, 108, 223, 38, 187, 44, 187, 197, 138, 143, 180, 248, 48, 132, 157, 193, 104, 196,
+        186, 26, 198, 214, 182, 119, 65, 187, 161, 108, 234, 95, 112, 36, 19, 42, 194, 95, 18, 45,
+        154, 203, 218, 118, 20, 185, 197, 197, 85, 42, 147, 251, 18, 197, 192, 107, 156, 191, 115,
+        194, 207, 162, 16, 69, 220, 173, 220, 211, 162, 84, 14, 167, 182, 91, 110, 178, 214, 31,
+        152, 103, 133,
+    ];
+    let mut block = Vec::new();
+
+    for i in 0..BLOCK_SIZE {
+        block.push(unsigned_block[i] as i8);
+    }
+
+    let mut iv = [0; BLOCK_SIZE];
+    generate_iv(&derived_key, &block.to_vec(), &mut iv);
+
+    let mut difference = [0; BLOCK_SIZE];
+    vector_difference(&block, &iv, &mut difference);
+
+    assert!(!are_orthogonal(&derived_key, &difference));
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::blocks::generation::vectors::are_orthogonal;
-    use crate::blocks::generation::vectors::vector_difference;
     use crate::blocks::SALT_SIZE;
 
     #[test]
@@ -79,28 +113,5 @@ mod tests {
         for e in derived_key.iter() {
             assert_eq!(PRIMES.iter().any(|p| p == e), true);
         }
-    }
-
-    #[test]
-    fn generate_iv_derived_key_is_not_orthogonal_with_block_and_key_difference() {
-        use rand::{thread_rng, RngCore};
-        let mut rng = thread_rng();
-
-        let derived_key = [1; BLOCK_SIZE];
-
-        let mut unsigned_block = [0; BLOCK_SIZE];
-        rng.fill_bytes(&mut unsigned_block);
-        let mut block = Vec::new();
-        for i in 0..BLOCK_SIZE {
-            block.push(unsigned_block[i] as i8);
-        }
-
-        let mut iv = [0; BLOCK_SIZE];
-        generate_iv(&derived_key, &block.to_vec(), &mut iv);
-
-        let mut difference = [0; BLOCK_SIZE];
-        vector_difference(&block, &iv, &mut difference);
-
-        assert!(!are_orthogonal(&derived_key, &difference));
     }
 }
